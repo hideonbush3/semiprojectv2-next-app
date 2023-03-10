92c7ec9aab8ce1f5c82ae1b0247e80329b44bd91
hideonbush3 <ppoii0961@gmail.com>
2023-03-10 11:00:43 +0900

    MariaDB용 Board 클래스 구현 2 - REST API board list

--- pages/api/board/list.js
+++ pages/api/board/list.js
@@ -1,33 +1,16 @@
-import mariadb from 'mariadb'
-
-// RESTAPI : http:/localhost:3000/api/board/list
-// view : http:/localhost:3000/board/list
-
-const dbconfig = {
-    host: process.env.MARIADB_HOST,
-    user: process.env.MARIADB_USER,
-    password: process.env.MARIADB_PWD,
-    database: process.env.MARIADB_DB,
-};
+import Board from '/models/Board';
 
 export default async (req, res) => {
-    let conn;
-    // limit first, second의 firsr는 start number이고 second는 가져올 개수
-    // 0번부터 15개 가져온다
-    const sql = `select bno, title, userid, date_format(regdate, "%Y-%m-%d") regdate, views from board order by bno desc limit 0, 15`;
+    let [ cpg, ftype, fkey ] = [ req.query.cpg, req.query.ftype, req.query.fkey ];
+    let stnum = (cpg - 1) * 25 + 1;  // 지정한 페이지 범위 시작값 계산
 
     try {
-        conn = await mariadb.createConnection(dbconfig);
-
-        const rowData = await conn.query(sql);
-        // 정상 처리 후 응답 : 상태코드 200, 조회결과 반환
-        res.status(200).json(rowData);  // json으로 응답
-
+        const rowData = new Board().select(stnum, ftype, fkey)
+            .then((result) => result);
+        res.status(200).json(await rowData);  // json으로 응답
     } catch (err) {
         console.log(err);
         // 처리 실패시 응답 : 상태코드 500, 오류메시지 반환
         res.status(500).json(err).end();
-    } finally {
-        if (conn) await conn.close();
     }
 }
