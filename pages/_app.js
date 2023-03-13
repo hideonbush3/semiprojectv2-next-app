af173f37ea5773cc265cda2858f602effca7872c
hideonbush3 <ppoii0961@gmail.com>
2023-03-13 11:46:10 +0900

    주석 수정

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,12 @@ import '../public/css/normalize.css'
 
 import React from "react";
 import Layout from "./layout";
+{/*MyApp 컴포넌트는 모든 페이지에서 공통적으로 사용되며,
+따라서 React 기반 웹 애플리케이션에서 가장 근본이 되는 컴포넌트 중 하나*/}
+{/*Layout 컴포넌트는 모든 페이지에서 공통적으로 사용될 레이아웃을 정의하는 컴포넌트*/}
+{/*<Component>는 Next.js에서 페이지 컴포넌트를 나타내는 특별한 프로퍼티*/}
+{/*이 프로퍼티는 Next.js에서 현재 페이지의 컴포넌트를 나타냅니다*/}
+{/*pageProps는 페이지 컴포넌트로 전달된 데이터를 나타내는 객체입니다.*/}
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/layout/Header.js
+++ pages/layout/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
             <li><Link href="/">Home</Link></li>
             <li><Link href="/member/Join">회원가입</Link></li>
             <li><Link href="/member/Login">로그인</Link></li>
-            <li><Link href="/board/List">게시판</Link></li>
+            <li><Link href="/board/list">게시판</Link></li>
             <li><Link href="/member/MyInfo">회원정보</Link></li>
           </ul>
           <hr />
